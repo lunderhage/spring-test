4f4d06a26abed8c56f9a37d37a1e30c19f660d70
Andreas Lunderhage <lunderhage@gmail.com>
2018-04-06 20:06:43 +0200

    Simple Application
        - It returns a bean serialized with jackson.

---
+++ src/main/java/se/lunderhage/spring/test/hello/Greeting.java
@@ -0,0 +1,28 @@
+package se.lunderhage.spring.test.hello;
+
+import java.util.Date;
+
+public class Greeting {
+
+    private final long id;
+    private final String content;
+    private final Date date;
+
+    public Greeting(long id, String content) {
+        this.id = id;
+        this.content = content;
+        this.date = new Date();
+    }
+
+    public long getId() {
+        return id;
+    }
+
+    public String getContent() {
+        return content;
+    }
+
+    public Date getDate() {
+        return date;
+    }
+}
